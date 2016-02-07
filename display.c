52c101a7251b0974aeccab29f1d884893b28fb09
Thu Hang <boongcd@yahoo.com>
2016-02-07 15:38:01 +0800

    change file name

--- display.c
+++ display.c
@@ -1,44 +1,5 @@
-/***************************** Include Files ********************************/
-
-#include "xtft.h"
-#include "xparameters.h"
-
-/************************** Constant Definitions ****************************/
-/**
- * The following constants map to the XPAR parameters created in the
- * xparameters.h file. They are defined here such that a user can easily
- * change all the needed parameters in one place.
- */
-#define TFT_DEVICE_ID    XPAR_TFT_0_DEVICE_ID
-#define DDR_HIGH_ADDR    XPAR_PS7_DDR_0_S_AXI_HIGHADDR 
-
-#ifndef DDR_HIGH_ADDR
-#warning "CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H"
-#endif
-
-#define DISPLAY_COLUMNS  640
-#define DISPLAY_ROWS     480
-
-
-/**
- * User has to specify a 2MB memory space for filling the frame data.
- * This constant has to be updated based on the memory map of the
- * system.
- */
-#define TFT_FRAME_ADDR        0x10000000
-
-/**************************** Type Definitions ******************************/
-
-/************************** Function Prototypes *****************************/
-
-int initTFT();
-void drawCircle(int x0, int y0, int r);
-void drawHorLine(XTft *Tft, int y, int x1, int x2, int col);
-
-
-/************************** Variable Definitions ****************************/
-
-static XTft TftInstance;
+#include "display.h"
+#include <math.h>
 
 int initTFT()
 {
@@ -95,7 +56,7 @@ void drawCircle(int x0, int y0, int r)
 	while (0<=x)
 	{
 		y++;
-		re =  x^2 + y^2 - r^2;
+		re =  pow(x,2) + pow(y,2) - pow(r,2);
 		yChange = 2*y+1;
 		xChange = 1-2*x;
 		dec = 2*(re+yChange)+xChange;
---
+++ display.h
@@ -0,0 +1,41 @@
+/***************************** Include Files ********************************/
+
+#include "xtft.h"
+#include "xparameters.h"
+
+/************************** Constant Definitions ****************************/
+/**
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+#define TFT_DEVICE_ID    XPAR_TFT_0_DEVICE_ID
+#define DDR_HIGH_ADDR    XPAR_PS7_DDR_0_S_AXI_HIGHADDR 
+
+#ifndef DDR_HIGH_ADDR
+#warning "CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H"
+#endif
+
+#define DISPLAY_COLUMNS  640
+#define DISPLAY_ROWS     480
+
+
+/**
+ * User has to specify a 2MB memory space for filling the frame data.
+ * This constant has to be updated based on the memory map of the
+ * system.
+ */
+#define TFT_FRAME_ADDR        0x10000000
+
+/**************************** Type Definitions ******************************/
+
+/************************** Function Prototypes *****************************/
+
+int initTFT();
+void drawCircle(int x0, int y0, int r);
+void drawHorLine(XTft *Tft, int y, int x1, int x2, int col);
+
+
+/************************** Variable Definitions ****************************/
+
+static XTft TftInstance;
