538732d5e13cfbe46e6051f739d93805068dd7bc
Thu Hang <boongcd@yahoo.com>
2016-02-07 11:35:41 +0800

    add initTFT()

--- TFT.c
+++ TFT.c
@@ -27,6 +27,66 @@
  */
 #define TFT_FRAME_ADDR        0x10000000
 
+/**************************** Type Definitions ******************************/
+
+/************************** Function Prototypes *****************************/
+
+int initTFT();
+void drawCircle(int x0, int y0, int r);
+void drawHorLine(XTft *Tft, int y, int x1, int x2, int col);
+
+
+/************************** Variable Definitions ****************************/
+
+static XTft TftInstance;
+
+int initTFT()
+{
+	int Status;
+  XTft_Config *TftConfigPtr;
+  unsigned int *col;
+
+  unsigned char c;
+
+  /*
+   * Get address of the XTft_Config structure for the given device id.
+   */
+  TftConfigPtr = XTft_LookupConfig(TFT_DEVICE_ID);
+  if (TftConfigPtr == (XTft_Config *)NULL) {
+    return XST_FAILURE;
+  }
+
+  /*
+   * Initialize all the TftInstance members and fills the screen with
+   * default background color.
+   */
+  Status = XTft_CfgInitialize(&TftInstance, TftConfigPtr,
+      TftConfigPtr->BaseAddress);
+  if (Status != XST_SUCCESS) {
+    return XST_FAILURE;
+  }
+
+  /*
+   * Wait till Vsync(Video address latch) status bit is set before writing
+   * the frame address into the Address Register. This ensures that the
+   * current frame has been displayed and we can display a new frame of
+   * data. Checking the Vsync state ensures that there is no data flicker
+   * when displaying frames in real time though there is some delay due to
+   * polling.
+   */
+  while (XTft_GetVsyncStatus(&TftInstance) !=
+      XTFT_IESR_VADDRLATCH_STATUS_MASK);
+
+  /*
+   * Change the Video Memory Base Address from default value to
+   * a valid Memory Address and clear the screen.
+   */
+  XTft_SetFrameBaseAddr(&TftInstance, TFT_FRAME_ADDR);
+  XTft_ClearScreen(&TftInstance);
+  
+  return XST_SUCCESS
+}
+
 void drawCircle(int x0, int y0, int r)
 {
 	int x = r;
